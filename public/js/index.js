a1d606b3e50d9b320ca437260114abd0b620d7b2
tamsharvey <tamsin1803@gmail.com>
2023-02-20 19:34:39 +0000

    Functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+const functions = require("firebase-functions");
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
