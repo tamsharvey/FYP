7c3aa28972f3676c39a004de47bdeafa0b542fd2
tamsharvey <tamsin1803@gmail.com>
2023-02-20 17:57:49 +0000

    FirebaseHalfWorking

--- public/js/index.js
+++ public/js/index.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCyEbtegGBSKBrX91PLT_fqEPuaJHY3fCk",
-  authDomain: " rateittknh.firebaseapp.com",
-  projectId: " rateittknh",
-  storageBucket: "rateittknh.appspot.com",
-  messagingSenderId: "332274706767",
-  appId: "1:332274706767:web:e622db1600ef01029abd3a",
-  measurementId: "G-Q34JZ7NRBN"
+  apiKey: "AIzaSyA1Hx44cww4Hs0GKsW-f5YyDwtOTW5hck4",
+  authDomain: "finalyearproject-4e560.firebaseapp.com",
+  databaseURL: "https://finalyearproject-4e560-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "finalyearproject-4e560",
+  storageBucket: "finalyearproject-4e560.appspot.com",
+  messagingSenderId: "245045305412",
+  appId: "1:245045305412:web:2e9894028ed49f2fc29c11",
+  measurementId: "G-Y6PZ2E3WQQ"
 };
 
 // Initialize Firebase
