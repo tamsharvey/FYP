b16a98a01bf31e9387261550a3ceebfe8bf9ede5
tamsharvey <tamsin1803@gmail.com>
2023-02-22 16:37:29 +0000

    SignInButton

--- funcs/index.js
+++ funcs/index.js
@@ -13,6 +13,40 @@ exports.uploaduserinfo = functions.https.onRequest((request, response) => {
     });
 });
 
+exports.uploadlocation = functions.https.onRequest((request, response) => {
+	cors(request, response, () => {
+
+		    return admin.firestore().collection('UserLocation').add(request.body).then((snapshot) => {
+            response.send("Saved in the database");
+        });
+    });
+});
+
+exports.getlocation = functions.https.onRequest((request, response) => {
+
+    cors(request, response, () => {
+        // 1. Connect to our Firestore database
+        let myData = [];
+        admin.firestore().collection('UserLocation').get().then((snapshot) => {
+
+            if (snapshot.empty) {
+                console.log('No matching documents.');
+                response.send('No users ');
+                return;
+            }
+
+            snapshot.forEach(doc => {
+                let docObj = {};
+                docObj.id = doc.id;
+                myData.push(Object.assign(docObj, doc.data()));
+            });
+
+            // 2. Send data back to client
+            response.send(myData);
+        })
+    });
+});
+
 //get users data
 exports.getusers = functions.https.onRequest((request, response) => {
 
@@ -38,5 +72,4 @@ exports.getusers = functions.https.onRequest((request, response) => {
         })
     });
 });
-
 
