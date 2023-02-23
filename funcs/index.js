98f4712b9aa3dc2ee020296e2b8e80dd116e59d5
tamsharvey <tamsin1803@gmail.com>
2023-02-23 21:27:22 +0000

    closerButNoWork

--- funcs/index.js
+++ funcs/index.js
@@ -2,67 +2,95 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const axios = require("ajax");
 const cors = require('cors')({origin: true});
+const express = require("express");
 admin.initializeApp();
 
+// exports.searchMovie = functions.https.onRequest((request, response) => {
+//     cors(request, response, () => {
+//         return admin.firestore().collection('UserData').add(request.body).then((snapshot) => {
+//             response.send("Saved in the database");
+//         });
+//     })
+// });
+
 exports.searchMovie = functions.https.onRequest((request, response) => {
-    cors(request, response, () => {
-        const movieTitle = request.query.title;
-        const apiKey = '53c1020b3a0e7aeb482d50f68918374e'; // Replace with your API key
+  cors(request, response, () => {
+    const title = request.query.title;
+    const apiKey = 'YOUR_API_KEY';
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}`;
 
-        const apiUrl = `https://us-central1-finalyearproject-837f4.cloudfunctions.net/searchMovie?title=${movieTitle}&apiKey=${apiKey}`;
-
-        axios.get(apiUrl)
-            .then((apiResponse) => {
-                response.status(200).send(apiResponse.data);
-            })
-            .catch((error) => {
-                console.error(error);
-                response.status(500).send('Error accessing API');
-            });
-    })
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error retrieving movie data from the server');
+        }
+      })
+      .then(data => {
+        const results = data.results;
+        if (results.length > 0) {
+          const movie = results[0];
+          return admin.firestore().collection('Movies').add({
+            title: movie.title,
+            releaseDate: movie.release_date,
+            overview: movie.overview
+          }).then((snapshot) => {
+            response.send("Movie saved to the database");
+          });
+        } else {
+          response.send("Movie not found");
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        response.status(500).send("Error searching for movie");
+      });
+  });
 });
 
-exports.uploaduserinfo = functions.https.onRequest((request, response) => {
-    cors(request, response, () => {
 
-        return admin.firestore().collection('UserData').add(request.body).then((snapshot) => {
-            response.send("Saved in the database");
-        });
-    });
-});
-
-exports.uploadlocation = functions.https.onRequest((request, response) => {
-    cors(request, response, () => {
-
-        return admin.firestore().collection('UserLocation').add(request.body).then((snapshot) => {
-            response.send("Saved in the database");
-        });
-    });
-});
-
-//get users data
-exports.getusers = functions.https.onRequest((request, response) => {
-
-    cors(request, response, () => {
-        // 1. Connect to our Firestore database
-        let myData = [];
-        admin.firestore().collection('UserData').get().then((snapshot) => {
-
-            if (snapshot.empty) {
-                console.log('No matching documents.');
-                response.send('No users ');
-                return;
-            }
-
-            snapshot.forEach(doc => {
-                let docObj = {};
-                docObj.id = doc.id;
-                myData.push(Object.assign(docObj, doc.data()));
-            });
-
-            // 2. Send data back to client
-            response.send(myData);
-        })
-    });
-});
-
+// exports.uploaduserinfo = functions.https.onRequest((request, response) => {
+//     cors(request, response, () => {
+//
+//         return admin.firestore().collection('UserData').add(request.body).then((snapshot) => {
+//             response.send("Saved in the database");
+//         });
+//     });
+// });
+//
+// exports.uploadlocation = functions.https.onRequest((request, response) => {
+//     cors(request, response, () => {
+//
+//         return admin.firestore().collection('UserLocation').add(request.body).then((snapshot) => {
+//             response.send("Saved in the database");
+//         });
+//     });
+// });
+//
+// //get users data
+// exports.getusers = functions.https.onRequest((request, response) => {
+//
+//     cors(request, response, () => {
+//         // 1. Connect to our Firestore database
+//         let myData = [];
+//         admin.firestore().collection('UserData').get().then((snapshot) => {
+//
+//             if (snapshot.empty) {
+//                 console.log('No matching documents.');
+//                 response.send('No users ');
+//                 return;
+//             }
+//
+//             snapshot.forEach(doc => {
+//                 let docObj = {};
+//                 docObj.id = doc.id;
+//                 myData.push(Object.assign(docObj, doc.data()));
+//             });
+//
+//             // 2. Send data back to client
+//             response.send(myData);
+//         })
+//     });
+// });
+//
--- public/func.py
+++ public/func.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+import requests
+from flask import Flask, request
 import tmdbsimple as tmdb
 import json
 
@@ -7,11 +8,20 @@ tmdb.API_KEY = '53c1020b3a0e7aeb482d50f68918374e'
 app = Flask(__name__)
 
 
-@app.route("./funcs/index.js")
+@app.route('./searchMovie', methods=['GET'])
 def searchMovie():
-    name = request.args.get('movieSearch')
-    print(name)
-    return
+    apiUrl = 'https://us-central1-finalyearproject-837f4.cloudfunctions.net/searchMovie?query=1&apiKey=53c1020b3a0e7aeb482d50f68918374e'
+
+    response = requests.get(apiUrl)
+
+    if response.status_code == 200:
+        response_data = {'status': 'success'}
+        status_code = 200
+    else:
+        response_data = {'status': 'error', 'message': 'API request failed'}
+        status_code = 500
+
+    return response_data, status_code
 
 
 if __name__ == "__main__":
