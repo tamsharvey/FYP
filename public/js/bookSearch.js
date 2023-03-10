953f8a2cb191f9a5d09d0f8c86dcdf26c8ab2be4
tamsharvey <tamsin1803@gmail.com>
2023-03-10 21:40:59 +0000

    Getting book id

--- public/js/bookSearch.js
+++ public/js/bookSearch.js
@@ -1,34 +1,85 @@
 
-
-const APIKEY = "dRo9mkNRDFoHrJcriDaKSaiPyuU0mzE7";
+const APIKEY = "dRo9mkNRDFoHrJcriDaKSaiPyuU0mzE7"; // NYT
 const API ="https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=dRo9mkNRDFoHrJcriDaKSaiPyuU0mzE7"
-const bMain = document.getElementById("book");
+const bMain = document.getElementById("content");
 const form = document.getElementById("bookForm");
 const search = document.getElementById("bookName");
+const G = "https://www.googleapis.com/books/v1/volumes?&key=AIzaSyCyEbtegGBSKBrX91PLT_fqEPuaJHY3fCk/";
 
-const books = document.getElementById('book');
+// initially get fav movies
+NYTBooks(API);
 
-fetch(API)
+async function NYTBooks(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
 
-    .then(data=> {
-        console.log(data);
+    console.log(respData);
 
-        data.results.map(bk =>{
-            console.log(bk.title);
+    showBooks(respData.results);
+}
+function showBooks(books) {
+    // clear main
+    bMain.innerHTML = "";
 
-            let a = document.createElement("a");
-            a.innerHTML = bk.title;
+    books.forEach((book) => {
+        const bookInfo = book.book_details[0];
+        const isbn = book.isbns[0].isbn10;
+        const id = getID(isbn);
+        console.log(id);
 
-            let b = document.createElement("a");
-            b.innerHTML = bk.description;
+        const img = 'https://books.google.com/books/content?id=' + id + '&printsec=frontcover&img=1&zoom=5&source=gbs_api';
 
-            books.appendChild(a);
-            books.appendChild(b);
-        })
+        const bookEL = document.createElement("div");
+        bookEL.classList.add("book");
+
+        bookEL.innerHTML = `
+            <div class="book">
+                <h3>${bookInfo.title} - By ${bookInfo.author}</h3>
+                <h3>Description:</h3>
+                ${bookInfo.description}
+            </div>
+            <img ${img}>
+
+        `;
+
+        bMain.appendChild(bookEL);
+    });
+}
+
+
+function getID(isbn) {
+    fetch('https://www.googleapis.com/books/v1/volumes/?q=isbn:' + isbn , {
+        method: 'get'
     })
+        .then(response => { return response.json(); })
+        .then(data => {
+            const bookID = data.items[0]?.id;
+            return bookID;
+        })
+        .catch(error => {
+            console.log(error);
+            // console.log('Google API Error: Defaulting to archival images for book #' + id + ' ISBN: ' + isbn);
+            // var index = id - 1;
+            // var img = archivedImages[index];
+            // $('#cover-' + id).attr('src', img);
+        });
+}
 
-
-// fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=dRo9mkNRDFoHrJcriDaKSaiPyuU0mzE7', {
+//         .then(response => { return response.json(); })
+//         .then(data => {
+//             let img = data.items[0].volumeInfo.imageLinks.thumbnail;
+//             img = img.replace(/^http:\/\//i, 'https://');
+//             $('#cover-' + id).attr('src', img);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             // console.log('Google API Error: Defaulting to archival images for book #' + id + ' ISBN: ' + isbn);
+//             // var index = id - 1;
+//             // var img = archivedImages[index];
+//             // $('#cover-' + id).attr('src', img);
+//         });
+// }
+// fetch(API, {
 //     method: 'get',
 // })
 //     .then(response => { return response.json(); })
@@ -38,6 +89,35 @@ fetch(API)
 //         updateBestSellers(nytimesArchive);
 //     });
 //
+//
+// function updateBestSellers(nytimesBestSellers) {
+//     nytimesBestSellers.results.forEach(function(book) {
+//         const isbn = book.isbns[0].isbn10;
+//         const bookInfo = book.book_details[0];
+//         const lastWeekRank = book.rank_last_week || 'n/a';
+//         const weeksOnList = book.weeks_on_list || 'New this week!';
+//         const listing =
+//             '<div id="' + book.rank + '" class="entry">' +
+//             '<p>' +
+//             '<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/387928/book%20placeholder.png" class="book-cover" id="cover-' + book.rank + '">' +
+//             '</p>' +
+//             '<h2><a href="' + book.amazon_product_url + '" target="_blank">' + bookInfo.title + '</a></h2>' +
+//             '<h4>By ' + bookInfo.author + '</h4>' +
+//             '<h4 class="publisher">' + bookInfo.publisher + '</h4>' +
+//             '<p>' + bookInfo.description + '</p>' +
+//             '<div class="stats">' +
+//             '<hr>' +
+//             '<p>Last Week: ' + lastWeekRank + '</p>' +
+//             '<p>Weeks on list: ' + weeksOnList + '</p>' +
+//             '</div>' +
+//             '</div>';
+//
+//         $('#best-seller-titles').append(listing);
+//         $('#' + book.rank).attr('nyt-rank', book.rank);
+//
+//         updateCover(book.rank, isbn);
+//     });
+// }
 // function updateBestSellers(nytimesBestSellers) {
 //     nytimesBestSellers.results.forEach(function(book) {
 //         main.innerHTML = "";
@@ -91,24 +171,7 @@ fetch(API)
 //     });
 // }
 // }
-// function updateCover(id, isbn) {
-//     fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn + "&key=AIzaSyBlArJNUOrqx7UXL5QXTvo_QqRnS7LB3VA", {
-//         method: 'get'
-//     })
-//         .then(response => { return response.json(); })
-//         .then(data => {
-//             let img = data.items[0].volumeInfo.imageLinks.thumbnail;
-//             img = img.replace(/^http:\/\//i, 'https://');
-//             $('#cover-' + id).attr('src', img);
-//         })
-//         .catch(error => {
-//             console.log(error);
-//             // console.log('Googel API Error: Defaulting to archival images for book #' + id + ' ISBN: ' + isbn);
-//             // var index = id - 1;
-//             // var img = archivedImages[index];
-//             // $('#cover-' + id).attr('src', img);
-//         });
-// }
+
 //
 // $(window).scroll(function (event) {
 //     const scroll = $(window).scrollTop();
