fc911c4010cd93cf69532d0ccc9681ca8f00a68f
tamsharvey <tamsin1803@gmail.com>
2023-03-27 02:49:03 +0100

    basic function

--- public/js/addToList.js
+++ public/js/addToList.js
@@ -1,6 +1,30 @@
+function addDataToFirestore() {
+    // Get the current user
+    const user = firebase.auth().currentUser;
+    if (user) {
+        // Get the user's ID
+        const userId = user.uid;
 
+        // Get a reference to the Firestore collection
+        const db = firebase.firestore();
+        const userDataRef = db.collection("UserData");
 
+        // Get the data you want to add
+        const data = {
+            field1: "value1",
+            field2: "value2",
+            userId: userId
+        };
 
-// module.exports = {
-//     addToList: addMovie
-// };
+        // Add the data to the Firestore collection
+        userDataRef.add(data)
+            .then(() => {
+                console.log("Data added to Firestore!");
+            })
+            .catch((error) => {
+                console.error("Error adding data to Firestore: ", error);
+            });
+    } else {
+        console.error("User not logged in.");
+    }
+}
